ee941d601746ab2179a95f407f1c3ec821c40bcd
emin <memin1598@gmail.com>
2023-06-18 13:49:30 +0300

    35.05

--- color/src/components/Brand.js
+++ color/src/components/Brand.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import { getContrastYIQ } from '../helpers'
 
 function Brand({brand}) {
   return (
     <div className="brand">
         <h5>{brand.title}</h5>
         <div className="brand-colors">
-            {brand.colors.map()}
+            {brand.colors.map(color=>(
+              <span style={{'--bgColor':`#${color}`,'--textColor':`${getContrastYIQ(color)}`}}>{color}</span>
+            ))}
         </div>
     </div>
   )
--- color/src/components/Content.js
+++ color/src/components/Content.js
@@ -4,13 +4,14 @@ import { useState } from 'react'
 import BrandsData from '../brands.json'
 import Brand from './Brand'
 
+
 function Content() {
     const brandsArray=[]
     Object.keys(BrandsData).map(key=>{
         brandsArray.push(BrandsData[key])
     })
     const [brands,setBrands]=useState(brandsArray)
-
+    const [selectedBrands,setSelectedBrands]=useState([])
   return (
     <div className="content">
         <header className='header'>
